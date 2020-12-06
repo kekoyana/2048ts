d201c96965f1997209f44f2dbe0c4458278c7f69
kekoyana <kekoyana0@gmail.com>
2020-12-06 14:27:19 +0900

    tileの格納

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log('hello world');
+class Board{
+  tiles: number[][];
+
+  constructor() {
+      this.tiles = [
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0]
+      ];
+  }
+}
+
+const board = new Board();
+console.log(board);
