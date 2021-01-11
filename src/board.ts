545c1c3be86206747c4523285f77d93d093fd69e
kekoyana <kekoyana0@gmail.com>
2021-01-11 23:39:09 +0900

    add_right_tileの実装

--- src/board.ts
+++ src/board.ts
@@ -8,6 +8,18 @@ export default class Board{
           [0, 0, 0, 0],
           [0, 0, 0, 0]
       ];
+      this.add_right_tile()
   }
 
+  public add_right_tile(): boolean {
+    let tile_num: number = Math.random() < 0.5 ? 2 : 4;
+    let shuffle = [0, 1, 2, 3].sort(() => { return Math.random() - 0.5 })
+    for (let i of shuffle) {
+      if(this.tiles[i][3] == 0) {
+        this.tiles[i][3] = tile_num;
+        return true;
+      }
+    }
+    return false;
+  }
 }
