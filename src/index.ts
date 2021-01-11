ae26e1e9161c693712c96ab79130b741826d4207
kekoyana <kekoyana0@gmail.com>
2021-01-12 01:04:22 +0900

    button入力待

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 import Board from './board';
 
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+readline.question('What is your name? ', (answer: any) => {
+  console.log(`Hello, ${answer}!`);
+  readline.close();
+});
+
 const board = new Board();
 console.log(board.to_s());
